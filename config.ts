423414c63faca955dc752aa6876d8fa68c8f42a6
Itslilblv <boges8405@gmail.com>
2026-02-14 03:00:41 +0300

    Create config.ts
    Added API configuration and game settings constants.

---
+++ config.ts
@@ -0,0 +1,11 @@
+// هذا الملف يحتوي على كل ما يتعلق بالربط السحابي
+export const API_CONFIG = {
+  endpoint: 'https://api.jsonbin.io/v3/b/698ec55bd0ea881f40b6b5e3', 
+  key: '$2a$10$zP2nglkzKrG1K81/D3uY1ejRP73C/zxIpE9QiFtYPOS2pU9saHnU.' 
+};
+
+export const GAME_SETTINGS = {
+  maxPlayers: 20,
+  questionTime: 15,
+  initialLives: 5
+};
